825d4ae063d86606bd8c42989f5552ac58d9c183
hkirschke <33073460+hkirschke@users.noreply.github.com>
2023-03-25 09:26:14 -0300

    fixes properties in dtos

--- ALTC.Json.API/Dtos/PostsDto.cs
+++ ALTC.Json.API/Dtos/PostsDto.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ALTC.Infra.Json.API.Dtos;
+﻿namespace ALTC.Infra.Json.API.Dtos;
 
 public sealed class PostsDto
 {
-    public long? UserId { get; set; }
-    public long? Id { get; set; }
+    public int? UserId { get; set; }
+    public int? Id { get; set; }
     public string? Title { get; set; }
     public string? Body { get; set; }
 }
--- ALTC.Json.API/Dtos/UserDto.cs
+++ ALTC.Json.API/Dtos/UserDto.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ALTC.Infra.Json.API.Dtos;
+﻿namespace ALTC.Infra.Json.API.Dtos;
 
 public sealed class UserDto
 {
-    public long Id { get; set; }
+    public int Id { get; set; }
     public string? Name { get; set; }
     public string? Username { get; set; }
     public string? Email { get; set; }
