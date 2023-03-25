69f24f45c6f9661509f4d11a3c5c719c6436495f
hkirschke <33073460+hkirschke@users.noreply.github.com>
2023-03-25 11:42:22 -0300

    created JsonPlaceHolderService

---
+++ ALTC.Application/Services/JsonPlaceHolderService.cs
@@ -0,0 +1,22 @@
+﻿using ALTC.Application.Interfaces.Infrastructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALTC.Application.Services;
+
+public sealed class JsonPlaceHolderService
+{
+    private readonly IJsonPlaceHolderProxy _jsonPlaceHolderProxy;
+    private readonly ICacheRepository _cacheRepository;
+
+    public JsonPlaceHolderService(IJsonPlaceHolderProxy jsonPlaceHolderProxy, ICacheRepository cacheRepository)
+    {
+        _jsonPlaceHolderProxy = jsonPlaceHolderProxy;
+        _cacheRepository = cacheRepository;
+    }
+
+
+}
