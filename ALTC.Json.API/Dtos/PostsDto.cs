a085223b89219e208667772a73dfcb44f1673d21
hkirschke <33073460+hkirschke@users.noreply.github.com>
2023-03-25 09:17:22 -0300

    Created postsDto

---
+++ ALTC.Json.API/Dtos/PostsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALTC.Infra.Json.API.Dtos;
+
+public sealed class PostsDto
+{
+    public long? UserId { get; set; }
+    public long? Id { get; set; }
+    public string? Title { get; set; }
+    public string? Body { get; set; }
+}
